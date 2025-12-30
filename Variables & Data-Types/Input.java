ab05fe793ba5c17195c76ae8ae1bf8af7fd6f8ff
Antor-Chowdhury <antor1347@gmail.com>
2025-12-31 03:49:09 +0600

    added how to take input in java

---
+++ Variables & Data-Types/Input.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        // --- next() takes a single word.
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // --- nextLine() takes multiple words.
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // --- nextInt() takes integer value.
+        int number = sc.nextInt();
+        System.out.println(number);
+    }
+
+}
