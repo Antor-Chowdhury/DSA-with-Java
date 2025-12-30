0021ed77ed480d0d617152a6783216eb5fd7fa15
Antor-Chowdhury <antor1347@gmail.com>
2025-12-31 03:12:27 +0600

    added data types

---
+++ Variables & Data-Types/DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static void main(String args[]) {
+        // Primitive Data Types
+
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = true;
+        double price = 10.5;
+        int number = 25;
+        // long --> used when the int number is too long
+        // double --> used when the float number is too long
+        short n = 240;
+    }
+}
