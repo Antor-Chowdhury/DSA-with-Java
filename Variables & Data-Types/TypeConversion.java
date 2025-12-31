8ba7ca0e68333997dfb612c89ac2ae65d4b6628a
Antor-Chowdhury <antor1347@gmail.com>
2025-12-31 21:36:06 +0600

    added type conversion example

---
+++ Variables & Data-Types/TypeConversion.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class TypeConversion {
+    public static void main(String args[]) {
+        // int a = 10;
+        // long b = a;
+
+        // type conversion not possible.
+        // long a = 10;
+        // int b = a;
+
+        // System.out.println(b);
+
+        Scanner sc = new Scanner(System.in);
+
+        // int number = sc.nextFloat(); // not possible.
+
+        float number = sc.nextInt();
+        System.out.println(number);
+    }
+}
