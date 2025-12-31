eca7a1df356b7b4ec73655142207b4bdffcb39e6
Antor-Chowdhury <antor1347@gmail.com>
2025-12-31 22:22:50 +0600

    updated type promotion example

--- Variables & Data-Types/TypePromotion.java
+++ Variables & Data-Types/TypePromotion.java
@@ -3,5 +3,10 @@ public class TypePromotion {
         char a = 'a';
         char b = 'b';
         System.out.println(b - a);
+
+        byte c = 5;
+        // byte d = c * 2; // wrong
+        byte d = (byte) (c * 2);
+        System.out.println(d);
     }
 }
